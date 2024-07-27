2978a14c51c57d18dabe197b8732cc828f4e96ee
WildfootW <wildfootw@wildfoo.tw>
2024-07-28 00:58:42 +0800

    feat: init estate module

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyleft (ɔ) 2024 wildfootw <wildfootw@wildfoo.tw>
+#
+# Distributed under terms of the MIT license.
+
+{
+    'name': 'estate',
+    'depends': [
+        'base',
+    ],
+}
