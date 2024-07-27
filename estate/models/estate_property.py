dc0c80209faeeb7ef3dbc265f267ea59253f99cc
WildfootW <wildfootw@wildfoo.tw>
2024-07-28 01:26:09 +0800

    feat(estate): update property model attributes

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -11,3 +11,19 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
+
+    name = fields.Char(required = True)
+    description = fields.Text()
+    postcode = fields.Char()
+    data_availability = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_prientation = fields.Selection(
+        selection = [("North", "North"), ("South", "South"), ("East", "East"), ("West", "West")]
+    )
