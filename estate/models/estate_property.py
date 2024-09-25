5341331b2c675e0ea04fe40d980ad645b3a4060f
WildfootW <wildfootw@wildfoo.tw>
2024-09-26 01:26:24 +0800

    feat(property): add conditional display and widget options to form views
    - Added conditional visibility for 'Sold' and 'Cancel' buttons in the
      property form based on the state field.
    - Made garden area and orientation fields invisible when there is no
      garden.
    - Prevented the creation and editing of property type from the form
      view by adding options to the property_type_id field.
    - Added color picker to tags by adding a color field to
      estate.property.tag and configuring tag_ids with the appropriate
      widget options.
    - Restricted adding new offers when the property state is 'Offer
      Accepted', 'Sold', or 'Canceled' by setting the offer_ids
      field to readonly.
    - Conditionally hid 'Accept' and 'Refuse' buttons in the offer
      form when the state is set.

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -113,6 +113,7 @@ class EstatePropertyTag(models.Model):
     _order = "name"
 
     name = fields.Char(required = True)
+    color = fields.Integer(string = "Color")
 
     _sql_constraints = [
         ('unique_tag_name', 'UNIQUE(name)', 'The tag name must be unique.')
